409656a2d6069666766360dacbb28acd3fde447f
Charles Shi <shicharles.107@gmail.com>
2020-04-21 13:20:26 +0800

    Added wiki and annotation classes

---
+++ src/main/java/edu/brown/cs/ading6_cshi18_jgong15_sshaw4/cito/data/Annotation.java
@@ -0,0 +1,4 @@
+package edu.brown.cs.ading6_cshi18_jgong15_sshaw4.cito.data;
+
+public class Annotation {
+}
---
+++ src/main/java/edu/brown/cs/ading6_cshi18_jgong15_sshaw4/cito/data/Wiki.java
@@ -0,0 +1,16 @@
+package edu.brown.cs.ading6_cshi18_jgong15_sshaw4.cito.data;
+
+import java.util.Date;
+import java.util.Set;
+
+public class Wiki {
+  private Set<Annotation> annotationSet;
+  private String html; // TODO: find the best representation for html
+  // Maybe have an html parser for wiki?
+
+
+  public Wiki() {
+    annotationSet = null;
+  }
+
+}
