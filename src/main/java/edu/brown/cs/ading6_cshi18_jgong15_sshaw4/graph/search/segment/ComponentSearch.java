f31e5f79f6e150a7c5817c130e6776598b716bfd
sageshoyu <seiji.shaw@gmail.com>
2020-04-28 15:59:18 -0700

    change tarjan output to lists of sets to avoid compressing when using streams when comp gens into a set

--- src/main/java/edu/brown/cs/ading6_cshi18_jgong15_sshaw4/graph/search/segment/ComponentSearch.java
+++ src/main/java/edu/brown/cs/ading6_cshi18_jgong15_sshaw4/graph/search/segment/ComponentSearch.java
@@ -3,10 +3,11 @@ package edu.brown.cs.ading6_cshi18_jgong15_sshaw4.graph.search.segment;
 import edu.brown.cs.ading6_cshi18_jgong15_sshaw4.graph.Vertex;
 import edu.brown.cs.ading6_cshi18_jgong15_sshaw4.graph.exception.GraphException;
 
+import java.util.List;
 import java.util.Set;
 
 public interface ComponentSearch<T, W> {
 
-  Set<Set<Vertex<T, W>>> search(Vertex<T, W> start) throws GraphException;
+  List<Set<Vertex<T, W>>> search(Vertex<T, W> start) throws GraphException;
 
 }
