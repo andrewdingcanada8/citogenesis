f06aefa5e7b05abbb644ba5c514286c26a2c9fa6
Andrew X. Ding <andrew_ding@brown.edu>
2020-04-30 05:43:43 +0800

    annotate-socket class

--- src/main/resources/static/js/annotate.js
+++ src/main/resources/static/js/annotate.js
@@ -1,10 +1,19 @@
-function newAnnotation() {
-    let newDiv = document.createElement("div");
-
-
-    document.body.appendChild(newDiv);
-}
-
-function annotationClick(annotationID) {
-
-}
+// $(document).ready(() => {
+//     console.log("hey something happened.");
+//     console.log("hey something happened.");
+//     console.log("hey something happened.");
+// });
+//
+// function newAnnotation() {
+//     let newDiv = document.createElement("div");
+//
+//     document.body.appendChild(newDiv);
+// }
+//
+// function setup_hover () {
+//     $(".a-link1").mouseover(function (evt) {
+//         evt.preventDefault();
+//         // window.location.href = "#"+anchor;
+//         window.location.href = "#annotation0";
+//     });
+// }
