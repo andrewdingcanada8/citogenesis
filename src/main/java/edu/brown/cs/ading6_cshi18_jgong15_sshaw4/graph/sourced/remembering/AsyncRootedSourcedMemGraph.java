e45475198878046559f62cc92189102cb64e9003
sageshoyu <seiji.shaw@gmail.com>
2020-05-06 16:48:32 -0700

    Document most of graph package

--- src/main/java/edu/brown/cs/ading6_cshi18_jgong15_sshaw4/graph/sourced/remembering/AsyncRootedSourcedMemGraph.java
+++ src/main/java/edu/brown/cs/ading6_cshi18_jgong15_sshaw4/graph/sourced/remembering/AsyncRootedSourcedMemGraph.java
@@ -1,9 +1,9 @@
 package edu.brown.cs.ading6_cshi18_jgong15_sshaw4.graph.sourced.remembering;
 
-import edu.brown.cs.ading6_cshi18_jgong15_sshaw4.graph.Deadend;
 import edu.brown.cs.ading6_cshi18_jgong15_sshaw4.graph.Vertex;
 import edu.brown.cs.ading6_cshi18_jgong15_sshaw4.graph.exception.GraphException;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
@@ -19,10 +19,19 @@ import java.util.stream.Collectors;
  */
 public abstract class AsyncRootedSourcedMemGraph<T, W> extends RootedSourcedMemGraph<T, W> {
 
+  /**
+   * Constructs a new AsyncRootedSourcedMemGraph.
+   * @param headVal stored value for root Vertex
+   * @param maxDepth maximum search depth
+   */
   public AsyncRootedSourcedMemGraph(T headVal, int maxDepth) {
     super(headVal, maxDepth);
   }
 
+  /**
+   * Concurrent BFS search algorithm.
+   * @throws GraphException Exception while obtaining neighboring vertices and edges.
+   */
   @Override
   protected void loadByBFS() throws GraphException {
     Map<T, Integer> dMap = this.getDepthMap();
@@ -47,13 +56,18 @@ public abstract class AsyncRootedSourcedMemGraph<T, W> extends RootedSourcedMemG
     }
   }
 
+  /**
+   * Returns reference to results of asynchronous vertex exploration process.
+   * @param v vertex of which to explore edges
+   * @return reference to finished edge computation
+   */
   private CompletableFuture<Set<Vertex<T, W>>> vertExplore(Vertex<T, W> v) {
     return CompletableFuture.supplyAsync(() -> {
       try {
         return v.getEdges().stream().map(e -> e.getDest()).collect(Collectors.toSet());
       } catch (GraphException e) {
         System.err.println("Graph exploration error: " + e.getMessage());
-        return Set.of(new Deadend<>(null));
+        return Collections.emptySet();
       }
     });
 
